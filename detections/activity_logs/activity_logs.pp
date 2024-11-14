0dc45a964e3fb23af74a3f2ce3864f4071982bc1
ParthaI <partha@turbot.com>
2024-11-14 11:40:35 +0700

    Added .github folder and did a cleanup

--- detections/activity_logs/activity_logs.pp
+++ detections/activity_logs/activity_logs.pp
@@ -10,17 +10,17 @@ detection_benchmark "activity_log_detections" {
   type = "detection"
   children = [
     detection.activity_logs_detect_virtual_networks_create_modify_delete,
-    detection.activity_logs_detect_vpn_connection_modify_delete,
-    detection.activity_logs_detect_network_security_group_modify_delete,
-    detection.activity_logs_detect_application_gateway_modify_delete,
-    detection.activity_logs_detect_application_security_group_modify_delete,
-    detection.activity_logs_detect_container_registry_create_delete,
-    detection.activity_logs_detect_firewall_modify_delete,
-    detection.activity_logs_detect_grant_permission_detection,
-    detection.activity_logs_detect_keyvault_modify_delete,
+    detection.activity_logs_detect_vpn_connections_modify_delete,
+    detection.activity_logs_detect_network_security_groups_modify_delete,
+    detection.activity_logs_detect_application_gateways_modify_delete,
+    detection.activity_logs_detect_application_security_groups_modify_delete,
+    detection.activity_logs_detect_container_registries_create_delete,
+    detection.activity_logs_detect_firewalls_modify_delete,
+    detection.activity_logs_detect_grant_permissions_detection,
+    detection.activity_logs_detect_keyvaults_modify_delete,
     detection.activity_logs_detect_keyvault_secrets_modify_delete,
-    detection.activity_logs_detect_virtual_network_modify_delete,
-    detection.activity_logs_detect_kubernetes_cluster_create_delete,
+    detection.activity_logs_detect_virtual_networks_modify_delete,
+    detection.activity_logs_detect_kubernetes_clusters_create_delete,
     detection.activity_logs_detect_kubernetes_pods_delete
   ]
 
@@ -35,40 +35,36 @@ detection_benchmark "activity_log_detections" {
 
 detection "activity_logs_detect_virtual_networks_create_modify_delete" {
   title       = "Azure Virtual Network Creation, Modification, and Deletion Detection"
-  description = "Detects events in Azure Activity Logs where virtual networks are created, updated, or deleted. This detection focuses on identifying potential unauthorized changes or critical modifications within the Azure environment."
+  description = "Detects Azure Activity Log events for virtual network creation, updates, or deletion, highlighting unauthorized or critical modifications in the Azure environment."
   severity    = "high"
   query       = query.activity_logs_detect_virtual_networks_create_modify_delete
 
-  # references = [
-  #   "https://docs.github.com/en/actions/creating-actions/setting-exit-codes-for-actions#about-exit-codes",
-  # ]
-
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_vpn_connection_modify_delete" {
+detection "activity_logs_detect_vpn_connections_modify_delete" {
   title       = "Azure VPN Connection Modification/Deletion Detection"
-  description = "Identifies Azure Activity Log events where a VPN connection is modified or deleted, providing visibility into potential unauthorized changes or critical network modifications."
+  description = "Detects Azure Activity Log events for VPN connection modifications or deletions, offering insight into possible unauthorized changes or critical network adjustments."
   severity    = "medium"
-  query       = query.activity_logs_detect_vpn_connection_modify_delete
+  query       = query.activity_logs_detect_vpn_connections_modify_delete
 
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_network_security_group_modify_delete" {
+detection "activity_logs_detect_network_security_groups_modify_delete" {
   title       = "Azure Network Security Groups Modifications/Deletions"
-  description = "Identifies Azure Activity Log events where network security group configurations, including NSGs and related security rules, are modified or deleted. Useful for monitoring changes that may impact network security posture."
+  description = "Detects Azure Activity Log events involving modifications or deletions of network security group configurations, including NSGs and associated security rules, helping to monitor changes that could impact network security posture."
   severity    = "medium"
-  query       = query.activity_logs_detect_network_security_group_modify_delete
+  query       = query.activity_logs_detect_network_security_groups_modify_delete
 
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_application_gateway_modify_delete" {
+detection "activity_logs_detect_application_gateways_modify_delete" {
   title       = "Azure Application Gateway Modified or Deleted"
   description = "Detects when a application gateway is modified or deleted."
   severity    = "medium"
-  query       = query.activity_logs_detect_application_gateway_modify_delete
+  query       = query.activity_logs_detect_application_gateways_modify_delete
 
   references = [
     "https://learn.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations",
@@ -77,11 +73,11 @@ detection "activity_logs_detect_application_gateway_modify_delete" {
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_application_security_group_modify_delete" {
+detection "activity_logs_detect_application_security_groups_modify_delete" {
   title       = "Azure Application Security Group Modified or Deleted"
-  description = "Detects when a application security group is modified or deleted."
+  description = "Detects modifications or deletions of an application gateway, providing insight into potential unauthorized changes or critical updates to application delivery and security configurations."
   severity    = "medium"
-  query       = query.activity_logs_detect_application_security_group_modify_delete
+  query       = query.activity_logs_detect_application_security_groups_modify_delete
 
   references = [
     "https://learn.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations",
@@ -90,11 +86,11 @@ detection "activity_logs_detect_application_security_group_modify_delete" {
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_container_registry_create_delete" {
+detection "activity_logs_detect_container_registries_create_delete" {
   title       = "Azure Container Registry Created or Deleted"
-  description = "Detects when a Container Registry is created or deleted."
+  description = "Detects the creation or deletion of a Container Registry, providing visibility into significant changes that may impact container management and deployment."
   severity    = "low"
-  query       = query.activity_logs_detect_container_registry_create_delete
+  query       = query.activity_logs_detect_container_registries_create_delete
 
   references = [
     "https://learn.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations",
@@ -103,11 +99,11 @@ detection "activity_logs_detect_container_registry_create_delete" {
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_firewall_modify_delete" {
+detection "activity_logs_detect_firewalls_modify_delete" {
   title       = "Azure Firewall Modified or Deleted"
-  description = "Detects when a firewall is created, modified, or deleted."
+  description = "Detects the creation, modification, or deletion of a firewall, highlighting potential changes that could impact network security and access controls."
   severity    = "medium"
-  query       = query.activity_logs_detect_firewall_modify_delete
+  query       = query.activity_logs_detect_firewalls_modify_delete
 
   references = [
     "https://learn.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations",
@@ -116,11 +112,11 @@ detection "activity_logs_detect_firewall_modify_delete" {
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_virtual_network_modify_delete" {
+detection "activity_logs_detect_virtual_networks_modify_delete" {
   title       = "Azure Virtual Network Modified or Deleted"
-  description = "Detects when a Virtual Network is modified or deleted in Azure."
+  description = "Detects modifications or deletions of a Virtual Network in Azure, providing visibility into changes that may affect network structure and connectivity."
   severity    = "medium"
-  query       = query.activity_logs_detect_virtual_network_modify_delete
+  query       = query.activity_logs_detect_virtual_networks_modify_delete
 
   references = [
     "https://learn.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations",
@@ -129,11 +125,11 @@ detection "activity_logs_detect_virtual_network_modify_delete" {
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_grant_permission_detection" {
+detection "activity_logs_detect_grant_permissions_detection" {
   title       = "Azure Permission Granted to an Account"
-  description = "Detects IPs from which users grant access to others on Azure resources, and raises an alert when an unfamiliar source IP address is used."
+  description = "Identifies IPs from which users grant access to others on Azure resources and alerts on access granted from previously unrecognized IP addresses, helping to flag potential unauthorized access attempts."
   severity    = "medium"
-  query       = query.activity_logs_detect_grant_permission_detection
+  query       = query.activity_logs_detect_grant_permissions_detection
 
   references = [
     "https://learn.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations",
@@ -142,11 +138,11 @@ detection "activity_logs_detect_grant_permission_detection" {
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_keyvault_modify_delete" {
+detection "activity_logs_detect_keyvaults_modify_delete" {
   title       = "Azure Key Vault Modified or Deleted"
   description = "Detects when a key vault is modified or deleted."
   severity    = "medium"
-  query       = query.activity_logs_detect_keyvault_modify_delete
+  query       = query.activity_logs_detect_keyvaults_modify_delete
 
   references = [
     "https://learn.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations",
@@ -168,11 +164,11 @@ detection "activity_logs_detect_keyvault_secrets_modify_delete" {
   tags = local.activity_log_detection_common_tags
 }
 
-detection "activity_logs_detect_kubernetes_cluster_create_delete" {
+detection "activity_logs_detect_kubernetes_clusters_create_delete" {
   title       = "Azure Kubernetes Cluster Created or Deleted"
   description = "Detects when a Azure Kubernetes Cluster is created or deleted."
   severity    = "medium"
-  query       = query.activity_logs_detect_kubernetes_cluster_create_delete
+  query       = query.activity_logs_detect_kubernetes_clusters_create_delete
 
   references = [
     "https://learn.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations",
@@ -214,11 +210,11 @@ query "activity_logs_detect_virtual_networks_create_modify_delete" {
       and
         status = 'Succeeded'
     order by
-      tp_timestamp desc;
+      timestamp desc;
   EOQ
 }
 
-query "activity_logs_detect_vpn_connection_modify_delete" {
+query "activity_logs_detect_vpn_connections_modify_delete" {
   sql = <<-EOQ
     select
       ${local.common_activity_logs_sql}
@@ -232,11 +228,11 @@ query "activity_logs_detect_vpn_connection_modify_delete" {
       and
         status = 'Succeeded'
     order by
-      tp_timestamp desc;
+      timestamp desc;
   EOQ
 }
 
-query "activity_logs_detect_network_security_group_modify_delete" {
+query "activity_logs_detect_network_security_groups_modify_delete" {
   sql = <<-EOQ
     SELECT
       ${local.common_activity_logs_sql}
@@ -253,11 +249,11 @@ query "activity_logs_detect_network_security_group_modify_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
-query "activity_logs_detect_application_gateway_modify_delete" {
+query "activity_logs_detect_application_gateways_modify_delete" {
   sql = <<-EOQ
     SELECT
       ${local.common_activity_logs_sql}
@@ -271,11 +267,11 @@ query "activity_logs_detect_application_gateway_modify_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
-query "activity_logs_detect_application_security_group_modify_delete" {
+query "activity_logs_detect_application_security_groups_modify_delete" {
   sql = <<-EOQ
     SELECT
       ${local.common_activity_logs_sql}
@@ -289,11 +285,11 @@ query "activity_logs_detect_application_security_group_modify_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
-query "activity_logs_detect_container_registry_create_delete" {
+query "activity_logs_detect_container_registries_create_delete" {
   sql = <<-EOQ
     SELECT
       ${local.common_activity_logs_sql}
@@ -307,11 +303,11 @@ query "activity_logs_detect_container_registry_create_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
-query "activity_logs_detect_firewall_modify_delete" {
+query "activity_logs_detect_firewalls_modify_delete" {
   sql = <<-EOQ
     SELECT
       ${local.common_activity_logs_sql}
@@ -325,11 +321,11 @@ query "activity_logs_detect_firewall_modify_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
-query "activity_logs_detect_grant_permission_detection" {
+query "activity_logs_detect_grant_permissions_detection" {
   sql = <<-EOQ
     SELECT
       ${local.common_activity_logs_sql}
@@ -340,11 +336,11 @@ query "activity_logs_detect_grant_permission_detection" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
-query "activity_logs_detect_keyvault_modify_delete" {
+query "activity_logs_detect_keyvaults_modify_delete" {
   sql = <<-EOQ
     SELECT
       ${local.common_activity_logs_sql}
@@ -360,7 +356,7 @@ query "activity_logs_detect_keyvault_modify_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
@@ -384,11 +380,11 @@ query "activity_logs_detect_keyvault_secrets_modify_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
-query "activity_logs_detect_virtual_network_modify_delete" {
+query "activity_logs_detect_virtual_networks_modify_delete" {
   sql = <<-EOQ
     SELECT
       ${local.common_activity_logs_sql}
@@ -404,11 +400,11 @@ query "activity_logs_detect_virtual_network_modify_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
-query "activity_logs_detect_kubernetes_cluster_create_delete" {
+query "activity_logs_detect_kubernetes_clusters_create_delete" {
   sql = <<-EOQ
     SELECT
       ${local.common_activity_logs_sql}
@@ -422,7 +418,7 @@ query "activity_logs_detect_kubernetes_cluster_create_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
 
@@ -437,6 +433,6 @@ query "activity_logs_detect_kubernetes_pods_delete" {
       AND
         status = 'Succeeded'
     ORDER BY
-      tp_timestamp DESC;
+      timestamp DESC;
   EOQ
 }
