9ffd2e71eb9f52f3a060702f79cf476864d9b10f
Cody Bruno <cody@turbot.com>
2025-01-30 13:07:01 -0500

    Fix mod color

--- mod.pp
+++ mod.pp
@@ -2,7 +2,7 @@ mod "azure_activity_log_detections" {
   # hub metadata
   title         = "Azure Activity Log Detections"
   description   = "Run detections and view dashboards for your Azure activity logs to monitor and analyze activity across your Azure subscriptions using Powerpipe and Tailpipe."
-  color         = "#191717"
+  color         = "#0089D6"
   documentation = file("./docs/index.md")
   icon          = "/images/mods/turbot/azure-activity-log-detections.svg"
   categories    = ["azure", "dashboard", "detections", "public cloud"]
